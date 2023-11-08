b82edb038d1c1894e1ba9593efe8d928bb29b718
Ocean <ocean@ocean.lol>
2023-11-08 02:59:06 +0000

    fix careen bug

--- src/main/java/oceanrodent/cards/Careen.java
+++ src/main/java/oceanrodent/cards/Careen.java
@@ -21,8 +21,6 @@ public class Careen extends AbstractRodentCard {
         baseMagicNumber = magicNumber = 1;
         baseSecondMagic = secondMagic = 6;
         exhaust = true;
-        previewsJunk = true;
-        cardsToPreview = dummyCard;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
